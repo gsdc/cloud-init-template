d7116c782639e57115c8b8c0c59ebd0574bd8fa0
Geonmo Ryu <geonmo@kisti.re.kr>
2022-12-05 15:37:01 +0900

    Create 00_install_role.sh

---
+++ 00_install_role.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum update -y ansible
+
+
+ansible-galaxy role install geerlingguy.repo-epel
+ansible-galaxy role install geonmo.ansible_htcondor
+ansible-galaxy role install geonmo.grid
+ansible-galaxy role install geonmo.htcondor_ce
