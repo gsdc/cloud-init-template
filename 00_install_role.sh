2d6694a7974b8f5ca104b1682d733220e8fe9651
Geonmo Ryu <geonmo@kisti.re.kr>
2022-12-12 14:24:37 +0900

    change role name

--- 00_install_role.sh
+++ 00_install_role.sh
@@ -4,6 +4,6 @@ sudo yum install -y ansible
 
 
 ansible-galaxy role install geerlingguy.repo-epel
-ansible-galaxy role install geonmo.ansible_htcondor
+ansible-galaxy role install geonmo.htcondor
 ansible-galaxy role install geonmo.grid
 ansible-galaxy role install geonmo.htcondor_ce
